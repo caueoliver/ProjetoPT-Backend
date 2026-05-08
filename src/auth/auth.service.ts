fd34bdd289a0577d0df5daa1f04f8c3b9193a161
caue-oliver <caue.ooliver@gmail.com>
2026-05-08 12:10:42 -0300

    feat: implementação do login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly UserService: UserService){}
+
     
+    constructor(
+        private readonly UserService: UserService,
+        private readonly jwtService: JwtService,
+    ){}
+    
+    login(user: User): UserToken {
+        const payload: UserPayload ={
+            sub: user.id,
+            email: user.email,
+            name: user.name,
+        };
+
+        const jwtToken = this.jwtService.sign(payload);
+
+        return{
+            access_token:jwtToken,
+        }
+    }
+
     async validateUser(email: string, password: string) {
         const user = await this.UserService.findbyEmail(email);
         
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+    id!: number;
+    email!: string;
+    name!: string;
+    username!: string;
+    password!: string;
+
+}
