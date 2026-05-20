79eba59f186cb1ef051f49677d9a18addcb09abd
gabrielsimplicioo <gabrielsimplicio043@gmail.com>
2026-05-20 20:36:33 -0300

    crud de produtos e de lojas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ProdutoModule } from './produto/produto.module';
+import { LojaModule } from './loja/loja.module';
 
 @Module({
-  imports: [UserModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    AuthModule,
+    ProdutoModule,
+    LojaModule
+  ],
   controllers: [AppController],
   providers: [AppService,
     {
